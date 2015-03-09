aef4d7ebad229cd20fce3c54b99c4a53c51aa702
trauko <f.f.quilodran@live.com>
2015-03-09 16:23:15 -0400

    hahaha

---
+++ LOG320Lab3/src/GameRules.java
@@ -0,0 +1,26 @@
+import java.util.List;
+
+
+public class GameRules
+{
+	private boolean canMoveInline()
+	{
+		return false;
+	}
+	private boolean canMoveInRow()
+	{
+		return false;
+	}
+	private boolean canMoveDiagonal()
+	{
+		return false;	
+	}
+	private boolean isCaseEmpty()
+	{
+		return false;
+	}
+	public List<String> generateMoves()
+	{
+		return null;
+	}
+}
