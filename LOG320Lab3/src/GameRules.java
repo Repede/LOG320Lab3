c1312b2e3f8acaf22aca730dd80462fecc28ce84
trauko <f.f.quilodran@live.com>
2015-03-15 15:17:13 -0400

    game rules

--- LOG320Lab3/src/GameRules.java
+++ LOG320Lab3/src/GameRules.java
@@ -3,22 +3,59 @@ import java.util.List;
 
 public class GameRules
 {
-	private boolean canMoveInline()
+	//Lorsque on trouve un endroit, on retourne la quantite de cases qu'on
+	//peut bouger.Il retour -1 lorsque il y a pas de movement.
+	private int canMoveInline(int[][] board, int currentLine)
 	{
-		return false;
+		int pawnCounter=0;
+		for (int i = 0; i < 0; i++)
+		{
+			if(!isCaseEmpty(board,currentLine,i))
+				pawnCounter++;
+		}
+		return pawnCounter;
 	}
-	private boolean canMoveInRow()
+	private int canMoveInRow(int[][] board, int currentRow)
 	{
-		return false;
+		int pawnCounter=0;
+		for (int i = 0; i < 0; i++)
+		{
+			if(!isCaseEmpty(board,i,currentRow))
+				pawnCounter++;
+		}
+		return pawnCounter;
 	}
-	private boolean canMoveDiagonal()
+	private int canMoveDiagonal(int[][] board, int currentRow)
 	{
-		return false;	
+		int pawnCounter=0;
+		for (int i = 0; i < 0; i++)
+		{
+			if(!isCaseEmpty(board,i,currentRow))
+				pawnCounter++;
+		}
+		return pawnCounter;
 	}
-	private boolean isCaseEmpty()
+	private int canMoveInverserDiagonal(int[][] board,int currentX,int currentY)
 	{
-		return false;
+		int pawnCounter=0;
+		//looking upper position from original position
+		while(currentX==0||currentY==7)
+		{
+			currentX--;
+			currentY++;
+		}
+		for (int i = 0; i < 0; i++)
+		{
+			//if(!isCaseEmpty(board,i,currentRow))
+				//pawnCounter++;
+		}
+		return pawnCounter;
 	}
+	private boolean isCaseEmpty(int[][] board, int xPosition,int yPosition )
+	{
+		return board[xPosition][yPosition]==0;
+	}
+
 	public List<String> generateMoves()
 	{
 		return null;
