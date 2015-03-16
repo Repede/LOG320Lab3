dd43983ba89812c23cf2a54ffdfeb20cab54b214
Olivier <Olivier@Olivier-PC>
2015-03-15 21:40:33 -0400

    Ajout package Game et de Test

--- LOG320Lab3/src/Game/Client.java
+++ LOG320Lab3/src/Game/Client.java
@@ -1,3 +1,4 @@
+package Game;
 import java.io.*;
 import java.net.*;
 
--- LOG320Lab3/src/Test/GenerateMovesTest.java
+++ LOG320Lab3/src/Test/GenerateMovesTest.java
@@ -1,13 +1,21 @@
 package Test;
+import Game.GameRules;
 import junit.framework.TestCase;
-
-
+ 
 public class GenerateMovesTest extends TestCase
-{
-
+{	
 	public void testGenerateMoves()
 	{
 		fail("Not yet implemented tsettettsett");
 	}
 
+	public void testCanMoveInline001(){
+		GameRules gr = new GameRules();
+		
+		//int[][] board = {
+				
+		//}
+		
+		//gr.canMoveInline(board, currentLine)
+	}
 }
