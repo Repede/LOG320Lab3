e6ea7d5c115981368f4c3c5a90339704f8be1641
trauko <f.f.quilodran@live.com>
2015-03-15 21:02:49 -0400

    oli bonne chance

--- LOG320Lab3/src/GameRules.java
+++ LOG320Lab3/src/GameRules.java
@@ -5,6 +5,13 @@ public class GameRules
 {
 	//Lorsque on trouve un endroit, on retourne la quantite de cases qu'on
 	//peut bouger.Il retour -1 lorsque il y a pas de movement.
+	public static final int BLACK_PAWN=2;
+	public static final int WHITE_PAWN=4;
+	public static final int EMPTY_PAWN=0;
+	public enum POSSIBLE_MOVES
+	{
+		RIGHT,LEFT,DIAGONAL,INVERSER_DIAGONAL
+	}
 	private int canMoveInline(int[][] board, int currentLine)
 	{
 		int pawnCounter=0;
@@ -25,12 +32,18 @@ public class GameRules
 		}
 		return pawnCounter;
 	}
-	private int canMoveDiagonal(int[][] board, int currentRow)
+	private int canMoveDiagonal(int[][] board,int currentX,int currentY)
 	{
 		int pawnCounter=0;
-		for (int i = 0; i < 0; i++)
+		//looking upper position from original position
+		while(currentX<0 || currentY>7)
 		{
-			if(!isCaseEmpty(board,i,currentRow))
+			currentX--;
+			currentY++;
+		}
+		while(currentX<8 && currentY>0)
+		{
+			if(!isCaseEmpty(board,currentX++,currentY--))
 				pawnCounter++;
 		}
 		return pawnCounter;
@@ -39,25 +52,56 @@ public class GameRules
 	{
 		int pawnCounter=0;
 		//looking upper position from original position
-		while(currentX==0||currentY==7)
+		while(currentX<7||currentY>0)
 		{
-			currentX--;
-			currentY++;
+			currentX++;
+			currentY--;
 		}
-		for (int i = 0; i < 0; i++)
+		while(currentX<0&&currentY>8)
 		{
-			//if(!isCaseEmpty(board,i,currentRow))
-				//pawnCounter++;
+			if(!isCaseEmpty(board,currentX--,currentY++))
+				pawnCounter++;
 		}
 		return pawnCounter;
 	}
 	private boolean isCaseEmpty(int[][] board, int xPosition,int yPosition )
 	{
-		return board[xPosition][yPosition]==0;
+		return board[xPosition][yPosition]==GameRules.EMPTY_PAWN;
 	}
-
-	public List<String> generateMoves()
+	private boolean isWhite(int[][] board, int xPosition,int yPosition)
 	{
+		return board[xPosition][yPosition]==GameRules.WHITE_PAWN;
+	}
+	//private boolean isPossibleToMove(board)
+
+	public List<String> generateMoves(int[][] board, int myColor)
+	{
+		for (int i = 0; i < 8; i++)
+			for (int j = 0; j < 8; j++)
+			{
+				//si c'est un pion on rentre
+				if(!this.isCaseEmpty(board, i, j))
+				{
+					//on verifie le nombre de cases que la piece peut avancer
+					int nbrPawnsInLine= this.canMoveInline(board, i);
+					int nbrPawnsInRows=this.canMoveInRow(board, j);
+					int nbrPawnsInDiagonal=this.canMoveDiagonal(board, i, j);
+					int nbrPawnsInInverserDiagonal=this.canMoveInverserDiagonal(board, i, j);
+					//on verifie si le mouvement est dans l'intervalle permi, 
+					//si la case n'est pas ocupper par un pion 
+					//si le existe un autre pion adversaire dans le chemin
+					
+					//Mouvement inline
+					if(nbrPawnsInLine>0)
+					{
+						
+					}
+					//if(nbrPawnsInLine<8)
+					
+					
+				}
+				
+			}
 		return null;
 	}
 }
