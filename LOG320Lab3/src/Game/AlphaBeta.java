c2f6cab33cf093c568212cf75abca5e65ff7b035
trauko <f.f.quilodran@live.com>
2015-03-25 20:09:48 -0400

    alpha beta

---
+++ LOG320Lab3/src/Game/AlphaBeta.java
@@ -0,0 +1,39 @@
+package Game;
+
+import java.util.Map.Entry;
+
+
+
+public class AlphaBeta
+{
+
+	public int alphabeta(MinMaxNode node,int alpha,int beta,boolean Player)
+	{
+		if(node.getChildren().isEmpty())
+		{
+		    return node.getValue();
+		}
+	    if (Player)
+	    {
+	    	for(Entry<Integer,MinMaxNode> child : node.getChildren().entrySet())
+	        {
+	        	alpha = Math.max(alpha, alphabeta(child.getValue(), alpha, beta, !Player ));     
+	            if (beta <=alpha)
+	                break;                           //  (* Beta cut-off *)
+	        }	        	
+	        return alpha;
+	    }
+	    else
+	    {
+	        for (Entry<Integer,MinMaxNode> child: node.getChildren().entrySet())
+	        {
+	        	beta = Math.min(beta, alphabeta(child.getValue(), alpha, beta, !Player));     
+	            if (beta <= alpha)
+	                break;                             
+	        }
+	        return beta;	
+	    }
+		
+	}         
+    
+}
